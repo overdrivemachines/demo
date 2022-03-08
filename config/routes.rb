98862d6acc68f35c73466cce3ffd042af3678bbd
Dipen Chauhan <get.dipen@gmail.com>
2022-03-08 14:16:58 -0800

    Annotated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
+# == Route Map
+#
+#         Prefix Verb   URI Pattern                                                                                       Controller#Action
+#          posts GET    /posts(.:format)                                                                                  posts#index
+#                POST   /posts(.:format)                                                                                  posts#create
+#       new_post GET    /posts/new(.:format)                                                                              posts#new
+#      edit_post GET    /posts/:id/edit(.:format)                                                                         posts#edit
+#           post GET    /posts/:id(.:format)                                                                              posts#show
+#                PATCH  /posts/:id(.:format)                                                                              posts#update
+#                PUT    /posts/:id(.:format)                                                                              posts#update
+#                DELETE /posts/:id(.:format)                                                                              posts#destroy
+#           root GET    /                                                                                                 post#index
+
 Rails.application.routes.draw do
   resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "post#index"
 end
